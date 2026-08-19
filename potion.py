e5c90c02074c31bb76f206ad573d541a41647f56
Antony prakasi <antonyprakasi2@gmail.com>
2026-08-19 23:01:40 +0530

    Add files via upload

---
+++ potion.py
@@ -0,0 +1,15 @@
+import random
+ingredients=["bloom boba","sunrays","twinkle neo","brownie ash"]
+effects=[
+"Makes you invisible!👻",
+"gives you everything that u seek🏰",
+"helps u to reach the magic world 🧿",
+"gives you feather⡞⠳⣄⣀⣠⠞⢷ ",
+"gives unlimited luck 🌿"
+]
+print("WELCOME TO THE MAGIC VERSE🔮")
+ingredient=random.choice(ingredients)
+effect=random.choice(effects)
+
+print("\n You added:",ingredient)
+print("Your potion:",effect)
